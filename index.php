d6d7f8e90d1eb8fb786305ec5762f417af667974
Abhi <khanal.abhi@me.com>
2013-01-01 00:51:17 -0500

    pg configured

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 function pg_connection_string()
 {
+	return "host=ec2-54-243-230-119.compute-1.amazonaws.com port=5432 dbname=d1bpcicqqckeef user=nldgwtgemzohlw password=gETRu_wde0XQGyoW7GA-I4gNfF sslmode=require options='--client_encoding=UTF8'";
 
 }
 
